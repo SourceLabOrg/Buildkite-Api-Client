1d83ff2e2ab3fb44efcaaef330a152cb1463aadf
Stephen Powis <stephen.powis@gmail.com>
2023-03-31 23:16:50 +0900

    resolve checkstyle and test failures

--- src/main/java/org/sourcelab/buildkite/api/client/util/BuildStateUtils.java
+++ src/main/java/org/sourcelab/buildkite/api/client/util/BuildStateUtils.java
@@ -1,19 +1,20 @@
 /**
  * Copyright 2023 SourceLab.org https://github.com/SourceLabOrg/Buildkite-Api-Client
- * <p>
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
  * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
  * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
  * persons to whom the Software is furnished to do so, subject to the following conditions:
- * <p>
+ *
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
  * Software.
- * <p>
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
  * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
  * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
  * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+
 package org.sourcelab.buildkite.api.client.util;
 
 import org.sourcelab.buildkite.api.client.response.BuildState;
@@ -39,8 +40,9 @@ public class BuildStateUtils {
             case RUNNING:
             case BLOCKED:
                 return true;
+            default:
+                return false;
         }
-        return false;
     }
 
     /**
@@ -61,7 +63,8 @@ public class BuildStateUtils {
             case NOT_RUN:
             case BROKEN:
                 return true;
+            default:
+                return false;
         }
-        return false;
     }
 }
